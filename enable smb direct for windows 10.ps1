a45c689a26e98fa468ef3d7409c6c78512492f0e
pennyberry <36544612+pennyberry@users.noreply.github.com>
2020-06-18 14:26:14 -0400

    Update enable smb direct for windows 10.ps1

--- enable smb direct for windows 10.ps1
+++ enable smb direct for windows 10.ps1
@@ -1,1 +1,4 @@
+#This will significantly speed up your transfer speeds between host and VMs running in Hyper-V
+#https://docs.microsoft.com/en-us/windows-server/storage/file-server/smb-direct
+#this is equivilant to enabling SMB Direct
 Enable-NetAdapterRdma -Name *
