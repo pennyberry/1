2840eedb0f36a4aa9f89816acd0030aca1d9d966
pennyberry <36544612+pennyberry@users.noreply.github.com>
2021-06-08 10:07:13 -0400

    Add files via upload

---
+++ InstallKubeCTL.ps1
@@ -0,0 +1,2 @@
+﻿#https://kubernetes.io/docs/tasks/tools/install-kubectl-windows/
+Invoke-WebRequest -Uri https://dl.k8s.io/release/v1.21.0/bin/windows/amd64/kubectl.exe -OutFile C:\windows\system32\kubectl.exe
